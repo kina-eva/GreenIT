ce462babf0ac00f35a480b32cd160d6239cb40c7
kina-eva <kina.el-ali@efrei.net>
2025-04-14 14:10:39 +0200

    2eme nv est une copie de undex

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-let billet = document.querySelector(".prix-billet");
+let billet = document.querySelector(".argent-total");
+let parsedBillet = parseFloat(billet.innerHTML); // on tilise celle ci
+
+let billetPrix = document.querySelector(".billet-prix");
+let parsedBilletPrix = parseFloat(billetPrix.innerHTML);
+//idem
+let billetNiveau = document.querySelector(".billet-niveau");
+let billetIncrease = document.querySelector(".billet-increase");
+let parsedBilletIncrease = parseFloat(billetIncrease.innerHTML);
+
+
+
 function incrementBillet() {
-    billet.innerHTML ++;
+    parsedBillet += 5;
+   
+    billet.innerHTML = Math.round(parsedBillet += parsedBillet);
+}
+
+function buyBillet(){
+    if(parsedBillet >= parsedBilletPrix){
+        parsedBillet -= parsedBilletPrix;
+
+        billetNiveau.innerHTML ++;
+//apres achat dun arbre ou banque
+        billetIncrease.innerHTML = parsedBilletIncrease;
+        parsedBillet += parsedBilletIncrease; // on augmente le nombre de billets par clic
+
+    }
 }
   
